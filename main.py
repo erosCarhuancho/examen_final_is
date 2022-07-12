a7a7f37e848153b1cda264cbc65551bd185b5af0
erosCarhuancho <eros.carhuancho@utec.edu.pe>
2022-07-12 11:03:02 -0500

    p3

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Prueba")
-print("Prueba2")
+from audioop import mul
+
+
+def multiplicar(numero1,numero2):
+    return numero1*numero2
+
+def sumar(numero1,numero2):
+    return numero1+numero2
+
+def main():
+    print(multiplicar(15,8))
+    print(sumar(15,8))
+    
+if __name__ == "__main__":
+    main()
